1f65f0d9b6202786e948ca9c2282b5720ca2a20e
S1mplyD <lucabennati2k16@gmail.com>
2022-06-13 21:26:21 +0200

    vs code extensions installation script

---
+++ vscodeExtInstall.sh
@@ -0,0 +1,8 @@
+code --install-extension ahmadawais.shades-of-purple
+code --install-extension CoenraadS.bracket-pair-colorizer-2
+code --install-extension esbenp.prettier-vscode
+code --install-extension Gruntfuggly.todo-tree
+code --install-extension llvm-vs-code-extensions.vscode-clangd
+code --install-extension PKief.material-icon-theme
+code --install-extension Tomi.xajssnippets
+code --install-extension aaron-bond.better-comments
