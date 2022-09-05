24f3bc80ecdec3f2106d9354bd3c28220261ce47
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2022-09-05 19:47:36 +0200

    Update vscodeExtInstall.sh

--- vscodeExtInstall.sh
+++ vscodeExtInstall.sh
@@ -1,18 +1,9 @@
-code --install-extension aaron-bond.better-comments
-code --install-extension abusaidm.html-snippets
-code --install-extension bradlc.vscode-tailwindcss
-code --install-extension CoenraadS.bracket-pair-colorizer-2
-code --install-extension dsznajder.es7-react-js-snippets
-code --install-extension ecmel.vscode-html-css
-code --install-extension esbenp.prettier-vscode
-code --install-extension Gruntfuggly.todo-tree
-code --install-extension llvm-vs-code-extensions.vscode-clangd
-code --install-extension mikestead.dotenv
-code --install-extension msjsdiag.vscode-react-native
-code --install-extension octref.vetur
-code --install-extension PKief.material-icon-theme
-code --install-extension RobbOwen.synthwave-vscode
-code --install-extension Tomi.xajssnippets
+#!bin/bash
+
+if command -v codium &> /dev/null
+then 
+
+echo INSTALLING VS CODIUM EXTENSIONS
 
 codium --install-extension aaron-bond.better-comments
 codium --install-extension abusaidm.html-snippets
@@ -29,3 +20,32 @@ codium --install-extension octref.vetur
 codium --install-extension PKief.material-icon-theme
 codium --install-extension RobbOwen.synthwave-vscode
 codium --install-extension Tomi.xajssnippets
+exit
+elif command -v code &> /dev/null
+then 
+
+echo INSTALLING VS CODE EXTENSIONS
+code --install-extension aaron-bond.better-comments
+code --install-extension abusaidm.html-snippets
+code --install-extension bradlc.vscode-tailwindcss
+code --install-extension CoenraadS.bracket-pair-colorizer-2
+code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension ecmel.vscode-html-css
+code --install-extension esbenp.prettier-vscode
+code --install-extension Gruntfuggly.todo-tree
+code --install-extension llvm-vs-code-extensions.vscode-clangd
+code --install-extension mikestead.dotenv
+code --install-extension msjsdiag.vscode-react-native
+code --install-extension octref.vetur
+code --install-extension PKief.material-icon-theme
+code --install-extension RobbOwen.synthwave-vscode
+code --install-extension Tomi.xajssnippets
+
+exit
+
+else
+
+echo VS CODE OR VS CODIUM NOT INSTALLED
+
+fi
+
